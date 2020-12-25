7e2a380ee11f4f0fe8cb08c0dba3f6fd49998727
dragon <ExplodingFKL@gmail.com>
2020-12-25 21:45:16 +0800

    init

---
+++ Server/src/main/kotlin/i/design/ClassDesignApplication.kt
@@ -0,0 +1,11 @@
+package i.design
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClassDesignApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClassDesignApplication>(*args)
+}
---
+++ Server/src/test/kotlin/i/design/ClassDesignApplicationTests.kt
@@ -0,0 +1,13 @@
+package i.design
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClassDesignApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
